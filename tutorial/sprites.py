79c4a76918be45d3e2d9ce16c3363e794f5910c5
Lucas Cardoso <lucas.gavirachi@edu.pucrs.br>
2022-08-23 23:56:50 -0300

    Fix camera bug

--- tutorial/sprites.py
+++ tutorial/sprites.py
@@ -94,10 +94,14 @@ class Player(pygame.sprite.Sprite):
                 if self.x_change > 0:
                     # Lines the top left corner of the sprites and then moves it to the left width amount, rewriting the player's position.
                     self.rect.x = hits[0].rect.left - self.rect.width
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.x += PLAYER_SPEED
                 # If moving left
                 if self.x_change < 0:
                     # Lines the top left corner of the sprites.
                     self.rect.x = hits[0].rect.right
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.x -= PLAYER_SPEED
         if direction == "y":
             hits = pygame.sprite.spritecollide(self, self.game.blocks, False)
             if hits:
@@ -105,10 +109,14 @@ class Player(pygame.sprite.Sprite):
                 if self.y_change > 0:
                     # Lines the top left corner of the sprites and then moves it to the left width amount, rewriting the player's position.
                     self.rect.y = hits[0].rect.top - self.rect.height
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.y += PLAYER_SPEED
                 # If moving left
                 if self.y_change < 0:
                     # Lines the top left corner of the sprites.
                     self.rect.y = hits[0].rect.bottom
+                    for sprite in self.game.all_sprites:
+                        sprite.rect.y -= PLAYER_SPEED
 
     def collide_enemy(self):
         hits = pygame.sprite.spritecollide(self, self.game.enemies, False)
